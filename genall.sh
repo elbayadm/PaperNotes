2d656a28e4036b0e9296476b62dcfc1b13d773bf
Maha <maha.elbayad@gmail.com>
2018-02-24 20:18:49 +0100

    auto up

--- genall.sh
+++ genall.sh
@@ -2,18 +2,16 @@
 # Basic if statement
 if [ $1 -gt 0 ]
 then
-    cd ~/github/PaperNotes/post/book_reading
+    cd ~/work/PaperNotes/post/book_reading
     ./gen_html_transfer.sh
-    cd ~/github/PaperNotes/post/Convolutional_Neural_Networks
+    cd ~/work/PaperNotes/post/Convolutional_Neural_Networks
     ./gen_html_transfer.sh
-    cd ~/github/PaperNotes/post/Hardware_Design
+    cd ~/work/PaperNotes/post/Hardware_Design
     ./gen_html_transfer.sh
-    cd ~/github/PaperNotes/post/Programming_Skills
+    cd ~/work/PaperNotes/post/Programming_Skills
     ./gen_html_transfer.sh
-    cd ~/github/PaperNotes/post/Recurrent_Neural_Network
+    cd ~/work/PaperNotes/post/Recurrent_Neural_Network
     ./gen_html_transfer.sh
-    cd ~/github/PaperNotes/post/Reinforcement_Learning
+    cd ~/work/PaperNotes/post/Reinforcement_Learning
     ./gen_html_transfer.sh
 fi
-cd ~/github/PaperNotes/post
-cp -rf ~/Dropbox/blog/github.io/2015-03-03-Datasets-Codes.md.html .
